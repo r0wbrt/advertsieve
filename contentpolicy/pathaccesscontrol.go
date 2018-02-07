ed43132958eec26d5e07777e120af033e82a2e68
Robert Christian Taylor <r0wbrt@gmail.com>
2018-02-06 23:30:13 -0600

    Fix bug in abp parsing code

--- contentpolicy/pathaccesscontrol.go
+++ contentpolicy/pathaccesscontrol.go
@@ -121,6 +121,7 @@ type abpRuleEvaluator struct {
 	isThirdPartyRequest bool
 }
 
+//Runs the request against the list of filters. Returns true if the path matched a block filter.
 func (abpFilter *PathAccessControl) EvaluateRequest(host string, path string, thirdParty bool, requestType int64) (blockPath bool, err error) {
 
 	var evaluator abpRuleEvaluator
@@ -326,6 +327,7 @@ func normalizeFilterDef(s string) (pattern, options string, isExceptionFilter bo
 	return
 }
 
+//Parses filter options following the $ symbol in a rule def.
 func (filterEntry *abpFilterEntry) parseOptions(options string, isExceptionFilter bool) bool {
 	//If an unhandled modifier is found and the filter is a block filter, that
 	//rule is thrown out. Rules which can not be completely and correctly
@@ -353,21 +355,6 @@ func (filterEntry *abpFilterEntry) parseOptions(options string, isExceptionFilte
 			continue
 		}
 
-		//Regular filters first. The ones that can not be made into exception
-		//modifiers (Have ~ in front of them)
-
-		switch mod {
-		case "match-case":
-			filterEntry.matchCase = true
-			continue
-		default:
-			if !isExceptionFilter {
-				return false
-			} else {
-				continue
-			}
-		}
-
 		var isNegateModifer bool = false
 
 		//Type filters go here.
@@ -381,6 +368,15 @@ func (filterEntry *abpFilterEntry) parseOptions(options string, isExceptionFilte
 		}
 
 		switch mod {
+
+		case "match-case":
+
+			//Invalid syntax, skip this rule
+			if isNegateModifer {
+				return false
+			}
+			filterEntry.matchCase = true
+
 		case "third-party":
 			if isNegateModifer {
 				filterEntry.applyToFirstPartyOnly = true
@@ -475,7 +471,6 @@ func (abpFilter *PathAccessControl) parseFilter(s string, filterEntry *abpFilter
 	}
 
 	return
-
 }
 
 func (filter *abpFilterEntry) compileRegex(s string) (isRegexFilter bool, err error) {
